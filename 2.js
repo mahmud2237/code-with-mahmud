de63f53e6232c5c83158e61455a3247a14e0903b
mahmud2237 <smahmud2237@gmail.com>
2024-03-13 20:11:27 +0600

    code 2 done

---
+++ 2.js
@@ -0,0 +1,6 @@
+console.log("This folder contains codes for the ultimate javascript course");
+var a = 67; // a contains 67
+console.log("The value of a is:", a);
+// update the value of a
+a = "Mahmud";
+console.log("The updated value of a is:", a);
