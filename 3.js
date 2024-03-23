a8afb9c1d94aca7e9115c6ae00442a44b37d7b00
mahmud2237 <smahmud2237@gmail.com>
2024-03-24 00:49:57 +0600

    consequences using var  in code

--- 3.js
+++ 3.js
@@ -1,6 +1,6 @@
 console.log("JavaScript code 3: var, let and const")
 
-// cons using var is below...nowadays var isn't using in Modern JavaScript Or ES6.
+// cons using var is below...nowadays var isn't using in Modern JavaScript Or ES6. So, always work with let and const.....
 var a = 45;
 var b = "Harry";
 var c = null;
