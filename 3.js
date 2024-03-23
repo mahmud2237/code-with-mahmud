ccbb717572508e65e5f13a911cb00af98c01e469
mahmud2237 <smahmud2237@gmail.com>
2024-03-24 00:37:22 +0600

    cons using var in code

---
+++ 3.js
@@ -0,0 +1,12 @@
+console.log("JavaScript code 3: var, let and const")
+
+// cons using var is below...nowadays var isn't using in Modern JavaScript Or ES6.
+var a = 45;
+var b = "Harry";
+var c = null;
+var d = undefined;
+{
+    var b = "Very";
+    console.log(b);
+}
+console.log(b , "---> What! So look at this problem using var");
