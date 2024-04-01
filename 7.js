2c7fc1434abea7792fba6eb58fca54d8db6f3b6f
mahmud2237 <smahmud2237@gmail.com>
2024-04-01 14:00:06 +0600

    conditional expression update code in JS

--- 7.js
+++ 7.js
@@ -2,8 +2,16 @@
 const prompt = require('prompt-sync')();
 const alert = require('prompt-sync')();
 
-let a = prompt("Enter your age : ")
-a = Number.parseInt(a)
-if (a>18){
-    alert("This is a valid number")
+let age = prompt("Enter your age : ")
+age = Number.parseInt(age)
+if (age>18){
+    alert("Great! You can drive car.")
 }
+else if(age<18 && age>=9){
+    alert("You can drive car from your 18.")
+}
+else{
+    alert("You are a kid.")
+}
+console.log("Done")
+console.log("You can", (age<18? "not drive" : "drive"), "the car")
