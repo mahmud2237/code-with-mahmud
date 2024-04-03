fb1108c7e77ebc14ccfec172758b5c34ec7c8cf6
mahmud2237 <smahmud2237@gmail.com>
2024-04-03 17:25:21 +0600

    For Loop code in Js

--- 9.js
+++ 9.js
@@ -9,9 +9,10 @@ let sum = 0
 let n = prompt("Enter the value of n : ")
 n = Number.parseInt(n)
 for (let i = 0; i < n; i++){
-    console.log(i+1);
+    sum += (i + 1)
+    console.log((i + 1), "+")
 }
-
+console.log("Sum of first " + n + " natural number is " + sum)
 
 
 
