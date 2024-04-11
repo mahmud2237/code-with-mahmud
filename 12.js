251d4a58481cbcd8dced10b488c2fd04e4f1d3e5
mahmud2237 <smahmud2237@gmail.com>
2024-04-12 00:04:53 +0600

    Chapter 3 Practice set solution

--- 12.js
+++ 12.js
@@ -2,6 +2,7 @@ const prompt = require("prompt-sync")()
 /**
  * Chapter 3 Practice set
  */
+
 // 1. Write a Js program to print the marks of a student in an object using for loop
 let marks = {
     Mahmud: 90,
@@ -12,25 +13,25 @@ let marks = {
 for(let i=0; i<Object.keys(marks).length; i++){
     console.log("The Marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]])
 }
+
 // 2. Write the program in Question 1 using for in loop
 for (let j in marks){
     console.log("The Marks of " + j + " are " + marks[j])
 }
+
 // 3. Write a Program to print "Try again" until the user enters the currect number
 let currectNumber = prompt("Enter Your desired currect number : ")
 let i
-while(i != currectNumber){
+while( i != currectNumber ){
     i = prompt("Guess the currect number => ")
     console.log('Try again')
 }
-console.log("Good Job! You have Entered the currect Number")
+console.log("Good Job! You have Entered the currect Number") // when get the currect number than this message will be print
+
 // 4. Write a function to find mean of 5 number
-function mean(a, b, c, d, e){
+
+const mean = (a, b, c, d, e) => {
     return (a + b + c + d + e) / 5;
 }
-a = 1
-b = 2 
-c = 3
-d = 4
-e = 5
-console.log(mean())
+
+console.log("Mean of those numbers are: ", mean(1, 2, 3, 4, 5))
