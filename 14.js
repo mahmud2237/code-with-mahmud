f98347f9fab08301963a53b2c8990e9d66def668
mahmud2237 <smahmud2237@gmail.com>
2024-05-17 06:31:58 +0600

    code updated

---
+++ 14.js
@@ -0,0 +1,7 @@
+// There are so many String Methods. Here are some of the JS String Methods
+let name = "Mahmud"
+
+console.log(name.toLowerCase())
+console.log(name.toUpperCase())
+console.log(name.slice(0 , 4))
+console.log(name.slice(4))
